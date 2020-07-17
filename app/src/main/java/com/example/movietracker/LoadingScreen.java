185f38714e4ded663f0f3af9635efdb21b2e234a
Smoky <57879188+KrSkander@users.noreply.github.com>
2020-07-17 04:07:37 +0200

    MovieTracker

---
+++ app/src/main/java/com/example/movietracker/LoadingScreen.java
@@ -0,0 +1,37 @@
+package com.example.movietracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LoadingScreen extends AppCompatActivity
+{
+    private static int SPLASH_TIME_OUT=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_loading_screen);
+
+        //remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_loading_screen);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+        public void run()
+        {
+            Intent homeIntent = new Intent (LoadingScreen.this, MainActivity.class);
+            startActivity(homeIntent);
+            finish();
+
+        }
+        },SPLASH_TIME_OUT);
+    }
+}
