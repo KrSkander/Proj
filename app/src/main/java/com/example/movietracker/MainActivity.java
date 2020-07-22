5d6f196e871a78e6ce4ae2cd63eba9edcd5de170
chiheb <chiheb.b.ammar97@gmail.com>
2020-07-22 02:37:44 +0200

    Delete Action bar on LoadingScreen color changes in colors.xml--ch

--- app/src/main/java/com/example/movietracker/MainActivity.java
+++ app/src/main/java/com/example/movietracker/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.movietracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String url;
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView =(WebView)findViewById(R.id.webView);
+
+        //inserer le site heberger:
+//        url = "https://www..com";
+
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle( View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl(url);
     }
 }
